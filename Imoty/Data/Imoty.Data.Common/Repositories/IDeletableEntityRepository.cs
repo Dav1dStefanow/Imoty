c0326f35060b6fdaec6138ce652b5ff10e01f01f
Dav1dStefanow <david.sstefanov@gmail.com>
2023-09-08 10:37:26 +0300

    Initial Upload

---
+++ Imoty/Data/Imoty.Data.Common/Repositories/IDeletableEntityRepository.cs
@@ -0,0 +1,18 @@
+﻿namespace Imoty.Data.Common.Repositories
+{
+    using System.Linq;
+
+    using Imoty.Data.Common.Models;
+
+    public interface IDeletableEntityRepository<TEntity> : IRepository<TEntity>
+        where TEntity : class, IDeletableEntity
+    {
+        IQueryable<TEntity> AllWithDeleted();
+
+        IQueryable<TEntity> AllAsNoTrackingWithDeleted();
+
+        void HardDelete(TEntity entity);
+
+        void Undelete(TEntity entity);
+    }
+}
---
+++ Imoty/Imoty.Common/GlobalConstants.cs
@@ -0,0 +1,9 @@
+﻿namespace Imoty.Common
+{
+    public static class GlobalConstants
+    {
+        public const string SystemName = "Imoty";
+
+        public const string AdministratorRoleName = "Administrator";
+    }
+}
